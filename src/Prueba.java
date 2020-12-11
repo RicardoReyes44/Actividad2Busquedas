411976786d4b75ab658adbc86c014304a7328cea
RicardoReyes44 <rickyboy1002@gmail.com>
2020-12-11 15:41:29 -0600

    Se agrego el menu y el manejo de excepciones

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 class BusquedaBinaria{
 	
 	public boolean binarySearch(long[] data, long target, int low, int high) {
-		
+	if (low > high)
+		return false;
+	else {
+		int mid = (low + high) / 2;
+		if (target == data[mid])
+			return true;
+		else if (target < data[mid])
+			return binarySearch(data, target, low, mid - 1);
+		else
+			return binarySearch(data, target, mid + 1, high);
+	}
 	}
 	
 }
@@ -10,7 +23,45 @@ class BusquedaBinaria{
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BusquedaBinaria bs = new BusquedaBinaria();
+		Scanner entrada = new Scanner(System.in);
+		
+		long array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+		
+		do {
+		try {
+			System.out.println("--------------MENU--------------");
+			System.out.println("1.- Busqueda Binaria");
+			System.out.println("2.- Busqueda Hash");
+			System.out.println("3.- Salir");
+			System.out.print("Introduce opcion: ");
+			byte opcion = entrada.nextByte();
+			
+			System.out.println("Array: " + Arrays.toString(array));
+			
+			if(opcion==1) {
+				
+				System.out.print("\nIntroduce numero a buscar: ");
+				long numero = entrada.nextLong();
+				
+			}else if(opcion==2) {
+
+				
+			}else if(opcion==3) {
+				
+				break;
+				
+			}else {
+				System.out.println("No existe esa opcion, prueba de nuevo");
+			}
+		}catch(InputMismatchException e) {
+			System.out.println("Error en la entrada de datos <" + e + ">, vuevle a intentarlo");
+		    entrada.nextLine();
+		}System.out.println();
+		}while(true);
+		
+		System.out.println("\n\nPrograma terminado");
 
 	}
 
